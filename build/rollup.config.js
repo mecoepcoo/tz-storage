df2800b6565a23bca040463e6a2b3d689ed53756
Tianzhen <340349237@qq.com>
2019-03-21 01:36:46 +0800

    init

---
+++ build/rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import {eslint} from 'rollup-plugin-eslint'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+import {uglify} from 'rollup-plugin-uglify'
+
+const env = process.env.NODE_ENV
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/tz-local-storage.js',
+    format: 'umd',
+    name: 'storage',
+    sourcemap: true,
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    }),
+    commonjs(),
+    nodeResolve(),
+    eslint(),
+    babel(),
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  ]
+}
---
+++ dist/tz-local-storage.js
@@ -0,0 +1,2 @@
+!function(n){"function"==typeof define&&define.amd?define(n):n()}(function(){"use strict";console.log(function n(){!function(n,o){if(!(n instanceof o))throw new TypeError("Cannot call a class as a function")}(this,n)})});
+//# sourceMappingURL=tz-local-storage.js.map
